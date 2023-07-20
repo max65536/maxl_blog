0a7f7816f1442e649420fd6c5005ef6ec0c10854
max65536 <maxinteger65536@gmail.com>
2023-07-20 23:10:03 +0800

    worked 1

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,5 @@
 #[macro_use] extern crate rocket;
 
-use rocket::data::ToByteUnit;
-use rocket::tokio::fs::File;
-use rocket::Data;
-use rocket::http::uri::Absolute;
-
-mod paste_id;
-use paste_id::PasteId;
-
-// #[get("/<id>")]
-// async fn retrieve(id: &str) -> Option<File> {
-//     let upload_dir = concat!(env!("CARGO_MANIFEST_DIR"), "/", "upload");
-//     let filename = Path::new(upload_dir).join(id);
-//     File::open(&filename).await.ok()
-// }
-
-#[get("/<id>")]
-async fn retrieve(id: PasteId<'_>) -> Option<File> {
-    File::open(id.file_path()).await.ok()
-}
-
-
-const ID_LENGTH:usize = 3;
-const HOST: Absolute<'static> = uri!("http://localhist:8000");
-
-#[post("/", data = "<paste>")]
-async fn upload(paste: Data<'_>) -> std::io::Result<String>{
-    let id = PasteId::new(ID_LENGTH);
-    paste.open(128.kibibytes()).into_file(id.file_path()).await?;
-    Ok(uri!(HOST, retrieve(id)).to_string())
-}
 
 #[get("/")]
 fn index() -> &'static str {
@@ -50,13 +20,14 @@ fn index() -> &'static str {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, retrieve, upload])
+    rocket::build().mount("/", routes![index, temp])
+        .attach(Template::fairing())
 }
 
-use rocket_dyn_templates::Template;
+use rocket_dyn_templates::{Template, context};
+// use tera::Context;
 
 #[get("/temp")]
 fn temp() -> Template {
-    let context = /* object-like value */;
-    Template::render("index", &context)
+    Template::render("base", context! { my_message: "value" })
 }
